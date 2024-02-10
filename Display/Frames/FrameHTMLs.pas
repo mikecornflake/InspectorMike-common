bcad8246d82c1d4595a24c63d84595a4be5fd94a
Mike Cornflake <mike.cornflake@gmail.com>
2024-02-10 14:19:17 +0800

    Bored of fixing this change between different LCL libraries

--- Display/Frames/FrameHTMLs.pas
+++ Display/Frames/FrameHTMLs.pas
@@ -51,7 +51,7 @@ Implementation
 
 {$R *.lfm}
 
-//Uses IpHtmlNodes;
+Uses IpHtmlNodes;  // leave in, even if compiler tells you this isn't used
 
 Constructor TFrameHTML.Create(TheOwner: TComponent);
 Begin
