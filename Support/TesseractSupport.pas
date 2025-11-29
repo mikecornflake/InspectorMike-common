1a68c9b1851a497949ba2321e9ada7c6e88679ff
Mike Cornflake <mike.cornflake@gmail.com>
2025-11-29 11:12:25 +0800

    Added support for Options
    Turns out tesseract CLI has a wide support for additional parameter.  Changes allows for user to provide their own, or to tweak a pre-built set of simplified options.

--- Support/TesseractSupport.pas
+++ Support/TesseractSupport.pas
@@ -9,15 +9,17 @@ Uses
 
 Function TesseractAvailable: Boolean;
 Function TesseractPath: String;
+Function BuildOptionsString(AOEM: Integer = 3; APSM: Integer = -1;
+  AWhitelist: String = ''): String;
 Procedure SetTesseractPath(AValue: String);
 Procedure InitializeTesseract;
 
-Function OCR(AFilename: String): String;
+Function OCR(AFilename: String; AOptions: String = ''): String;
 
 Implementation
 
 Uses
-  Forms, StringSupport, FileUtil, OSSupport;
+  Forms, StringSupport, FileUtil, OSSupport, FileSupport;
 
 Var
   FTesseractPath: String;
@@ -29,8 +31,12 @@ End;
 
 Function TesseractPath: String;
 Begin
-  Result := FTesseractPath;
+  Result := IncludeTrailingBackslash(FTesseractPath);
+End;
 
+Function GetTesseractExe: String;
+Begin
+  Result := IncludeTrailingBackslash(FTesseractPath) + 'tesseract' + GetExeExt;
 End;
 
 Procedure SetTesseractPath(AValue: String);
@@ -41,36 +47,108 @@ Begin
     FTesseractPath := '';
 End;
 
+Function BuildOptionsString(AOEM: Integer; APSM: Integer; AWhitelist: String): String;
+Begin
+  Result := '';
+
+  // For full explanation and further options see:
+  //   https://github.com/tesseract-ocr/tesseract/blob/main/doc/tesseract.1.asc
+
+  // OCR Engine Mode
+  //   -1 = Don't use this setting
+  //    0 = Original Tesseract only.
+  //    1 = Neural nets LSTM only.
+  //    2 = Tesseract + LSTM.
+  //    3 = Default, based on what is available.
+
+  If (AOEM >= 0) And (AOEM <= 3) Then
+    Result += Format('--oem %d ', [AOEM]);
+
+  // Page Segmentation Mode
+  //   -1 = Don't use this setting
+  //    0 = Orientation and script detection (OSD) only.
+  //    1 = Automatic page segmentation with OSD.
+  //    2 = Automatic page segmentation, but no OSD, or OCR. (not implemented)
+  //    3 = Fully automatic page segmentation, but no OSD. (Default)
+  //    4 = Assume a single column of text of variable sizes.
+  //    5 = Assume a single uniform block of vertically aligned text.
+  //    6 = Assume a single uniform block of text.
+  //    7 = Treat the image as a single text line.
+  //    8 = Treat the image as a single word.
+  //    9 = Treat the image as a single word in a circle.
+  //   10 = Treat the image as a single character.
+  //   11 = Sparse text. Find as much text as possible in no particular order.
+  //   12 = Sparse text with OSD.
+  //   13 = Raw line. Treat the image as a single text line,
+  //        bypassing hacks that are Tesseract-specific.
+
+  If (APSM >= 0) And (APSM <= 13) Then
+    Result += Format('--psm %d ', [APSM]);
+
+  // Whitelist
+  // Whitelist is set as a CONFIGVAR, so we use the -c parameter:
+  // -c CONFIGVAR=VALUE
+  //  Set value for parameter CONFIGVAR to VALUE. Multiple -c arguments are allowed.
+
+  // For a full list of ConfigVar's, run
+  // > tesseract --print-parameters
+
+  If AWhiteList <> '' Then
+    Result += Format('-c "tessedit_char_whitelist=%s" ', [AWhitelist]);
+
+  Result := Trim(Result);
+End;
+
 Procedure InitializeTesseract;
 Begin
   If FTesseractPath = '' Then
   Begin
-    // By default, use the ffpmeg folder distributed with the app
-    FTesseractPath := IncludeTrailingBackslash(Application.Location) + 'Tesseract-OCR';
+    // TODO: This search needs rationalising and adding to FileSupport...
+
+    // By default, use the OCR folder distributed with the app
+    FTesseractPath := ExpandFileName(IncludeTrailingBackslash(Application.Location) +
+      'Tesseract-OCR');
+    If DirectoryExists(FTesseractPath) Then
+      Exit;
+
+    // Maybe it was installed in the Apps folder?
+    FTesseractPath := ExpandFileName(IncludeTrailingBackslash(Application.Location) +
+      'Apps\Tesseract-OCR');
     If DirectoryExists(FTesseractPath) Then
       Exit;
 
     // Maybe it was installed in the same folder as the app?
-    FTesseractPath := IncludeTrailingBackslash(Application.Location) + '..\Tesseract-OCR';
+    FTesseractPath := ExpandFileName(IncludeTrailingBackslash(Application.Location) +
+      '..\Tesseract-OCR');
+    If DirectoryExists(FTesseractPath) Then
+      Exit;
+
+    // Maybe it was installed in the Apps folder?
+    FTesseractPath := ExpandFileName(IncludeTrailingBackslash(Application.Location) +
+      '..\Apps\Tesseract-OCR');
     If DirectoryExists(FTesseractPath) Then
       Exit;
 
     // Oh well, search the evironment PATH for the exe...
     FTesseractPath := FindDefaultExecutablePath(Format('tesseract%s', [GetExeExt]));
 
-    If DirectoryExists(FTesseractPath) Then
-      Exit;
-
-    FTesseractPath := '';
+    If FTesseractPath <> '' Then
+      FTesseractPath := IncludeTrailingBackslash(ExtractFileDir(FTesseractPath));
   End;
 End;
 
-Function OCR(AFilename: String): String;
+Function OCR(AFilename: String; AOptions: String): String;
 Var
   sCommand: String;
-  sTempDir, sOutput: String;
+  sTempDir, sOutputBase, sOutputFile: String;
   oStrings: TStringList;
 Begin
+  // AOptions are image specific
+
+  // either read https://github.com/tesseract-ocr/tesseract/blob/main/doc/tesseract.1.asc
+  // and supply your own options string
+  // or use BuildOptionsString() - Uses only a simplified subset
+
   If TesseractAvailable Then
   Begin
     sTempDir := IncludeTrailingBackslash(SysUtils.GetTempDir(False)) +
@@ -78,21 +156,29 @@ Begin
 
     ForceDirectories(sTempDir);
 
-    DeleteFile(sTempDir + '\OCR.txt');
-    sOutput := sTempDir + '\OCR';
+    // TODO: Investigate threadsafe locks
+    sOutputFile := UniqueFilename(sTempDir, 'OCR', '.txt', True);
 
-    sCommand := Format('%s\tesseract%s "%s" "%s"', [FTesseractPath, GetExeExt,
-      AFilename, sOutput]);
+    // Huh.  Tesseract must be automatically adding the .txt...
+    sOutputBase := ChangeFileExt(sOutputFile, '');
+
+    sCommand := Format('"%s" "%s" "%s" %s', [GetTesseractExe, AFilename, sOutputBase, AOptions]);
 
     RunEx(sCommand, nil, True);
 
-    oStrings := TStringList.Create;
-    Try
-      oStrings.LoadFromFile(sOutput + '.txt');
-      Result := oStrings.Text;
-    Finally
-      oStrings.Free;
-    End;
+    If FileExists(sOutputFile) Then
+    Begin
+      oStrings := TStringList.Create;
+      Try
+        oStrings.LoadFromFile(sOutputFile);
+        Result := oStrings.Text;
+      Finally
+        oStrings.Free;
+      End;
+      SysUtils.DeleteFile(sOutputFile);
+    End
+    Else
+      Result := '';
   End;
 End;
 
