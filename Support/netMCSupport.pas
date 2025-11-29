3a4b6ac34c084a5ebdc7013b293c615e9bcab3fc
Mike Cornflake <mike.cornflake@gmail.com>
2025-11-29 17:57:02 +0800

    Update Pascal headers to LGPL

--- Support/netMCSupport.pas
+++ Support/netMCSupport.pas
@@ -21,20 +21,20 @@ Unit netMCSupport;
   License
     This file is part of IM_units.lpk.
 
-    It is free software: you can redistribute it and/or modify it under the
-    terms of the GNU General Public License as published by the Free Software
-    Foundation, either version 3 of the License, or (at your option) any
-    later version.
+    This library is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at
+    your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    This library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+    General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program.  If not, see <https://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library. If not, see <https://www.gnu.org/licenses/>.
 
-    SPDX-License-Identifier: GPL-3.0-or-later
+    SPDX-License-Identifier: LGPL-3.0-or-later
 -------------------------------------------------------------------------------}
 
 {$mode objfpc}{$H+}
