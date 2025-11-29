47c2a076672f49032045f56cbb9ebc6dd7ce284a
Mike Cornflake <mike.cornflake@gmail.com>
2025-11-29 18:54:55 +0800

    Add IM_application headers to Pascal units

--- Display/Dialogs/DialogSQLFilter.pas
+++ Display/Dialogs/DialogSQLFilter.pas
@@ -1,5 +1,43 @@
 Unit DialogSQLFilter;
 
+{-------------------------------------------------------------------------------
+  Package   : IM_application
+  Unit      : DialogSQLFilter.pas
+  Description
+    Dialog form for composing and applying SQL dataset filters.
+
+  Source
+    Copyright (c) 2025
+    Inspector Mike 2.0 Pty Ltd
+    Mike Thompson (mike.cornflake@gmail.com)
+
+  History
+    ~2008: Creation date unknown, original local SVN repository lost
+    2014-07-05: Uploaded to SourceForge/Package "Shared"
+    2024-01-22: Migrated to Github.  Refactored package to "IM_application"
+    2025-11-29: Added LGPL-3.0-or-later license header
+
+  License
+    This file is part of IM_application.lpk.
+
+    This library is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+    General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library. If not, see <https://www.gnu.org/licenses/>.
+
+    SPDX-License-Identifier: LGPL-3.0-or-later
+-------------------------------------------------------------------------------}
+
+
+
 {$mode objfpc}
 
 Interface
